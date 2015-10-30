9ed8fcf1d61548a81ebc72f53d7d80a120164886
Joshua Plicque <plicjo@gmail.com>
2015-10-30 00:12:41 -0400

    Add CurrentConditions

--- hfdp/ruby/ch_02/solution/lib/current_conditions.rb
+++ hfdp/ruby/ch_02/solution/lib/current_conditions.rb
@@ -1,2 +1,11 @@
 class CurrentConditions
+  attr_accessor :temperature
+
+  def initialize
+    @temperature = nil
+  end
+
+  def update(weather_data)
+    self.temperature = weather_data.temperature
+  end
 end
